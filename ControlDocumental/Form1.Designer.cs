28e376730d2d8141201b7a4afeabf892d4f83fdb
Jacqueline Vasquez <j.vasquez0216@gmail.com>
2019-12-24 12:48:24 -0300

    primer commit

---
+++ ControlDocumental/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace ControlDocumental
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(27, 35);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(20, 20);
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(570, 537);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 72);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(524, 461);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ingreso OC";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.comboBox2);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.comboBox3);
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Location = new System.Drawing.Point(4, 72);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(562, 461);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Ingreso EP";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 72);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(524, 461);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Mantenedor";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(268, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(228, 39);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 32);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Tipo Producto";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(268, 204);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(228, 38);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 32);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Cantidad";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(268, 140);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 38);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 32);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Folio OC";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 65);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(256, 247);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(241, 39);
+            this.comboBox2.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(29, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(198, 32);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Folio EP final";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(264, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(212, 32);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "labeResultado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 32);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Folio EP inicial";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(261, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(212, 32);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "labeResultado";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(29, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(205, 32);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Tipo Producto";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(256, 59);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(241, 39);
+            this.comboBox3.TabIndex = 11;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(29, 62);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(135, 32);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Folio OC";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(149, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 65);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Ingresar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 595);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Centro Documental";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button2;
+
+    }
+}
+
---
+++ ControlDocumental/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ControlDocumental.Properties;
+using System.IO;
+
+ 
+namespace ControlDocumental
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string fileNAme = "C:\\BDCD\\bdCD.mdb";
+            if (!existDB(fileNAme))
+            {
+                bool realiza = CreateNewAccessDatabase(fileNAme);
+                if(realiza){
+                    MessageBox.Show("Se ha creado la BD con éxito");
+                }
+                else
+                {
+                    MessageBox.Show("Ha ocurrido un error al crear la BD");
+                }
+            }
+            else
+            {
+                MessageBox.Show("La BD ya existe");
+            }
+            
+        }
+
+       
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        public bool existDB(string curFile)
+        {
+            return File.Exists(curFile);
+        }
+
+        public bool CreateNewAccessDatabase(string fileName)
+        {
+            bool result = false;
+
+            ADOX.Catalog cat = new ADOX.Catalog();
+            ADOX.Table tbl_OC = new ADOX.Table();
+            ADOX.Table tbl_EP = new ADOX.Table();
+            ADOX.Table tbl_pesoDoc = new ADOX.Table();
+            ADOX.Table tbl_tipoProd = new ADOX.Table();
+            ADOX.Table tbl_tipoDoc = new ADOX.Table();
+
+            //CREA TABLA tbl_OC
+            tbl_OC.Name = "tbl_OC";
+            tbl_OC.Columns.Append("OC", ADOX.DataTypeEnum.adVarWChar, 15);
+            tbl_OC.Columns.Append("F_Ini", ADOX.DataTypeEnum.adVarWChar, 10);
+            tbl_OC.Columns.Append("F_Fin", ADOX.DataTypeEnum.adVarWChar, 10);
+            tbl_OC.Columns.Append("fecha", ADOX.DataTypeEnum.adDBDate, 50);
+
+            //CREA TABLA tbl_EP
+            tbl_EP.Name = "tbl_EP";
+            tbl_EP.Columns.Append("id_OC", ADOX.DataTypeEnum.adNumeric, 10);
+            tbl_EP.Columns.Append("F_Ini", ADOX.DataTypeEnum.adVarWChar, 10);
+            tbl_EP.Columns.Append("F_Fin", ADOX.DataTypeEnum.adVarWChar, 10);
+            tbl_EP.Columns.Append("fecha", ADOX.DataTypeEnum.adDBDate, 50);
+            tbl_EP.Columns.Append("hora", ADOX.DataTypeEnum.adDBTime, 50);
+
+            //CREA TABLA tbl_tipoDoc
+            tbl_tipoDoc.Name = "tbl_tipoDoc";
+            tbl_tipoDoc.Columns.Append("producto", ADOX.DataTypeEnum.adVarWChar, 10);
+            
+            
+            //CREA TABLA tbl_pesoDoc
+            tbl_pesoDoc.Name = "tbl_pesoDoc";
+            tbl_pesoDoc.Columns.Append("id_form", ADOX.DataTypeEnum.adNumeric, 10);
+            tbl_pesoDoc.Columns.Append("cantidad", ADOX.DataTypeEnum.adNumeric, 10);
+            tbl_pesoDoc.Columns.Append("peso", ADOX.DataTypeEnum.adDouble, 5);
+
+
+            try
+            {
+                cat.Create("Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + fileName + "; Jet OLEDB:Engine Type=5");
+                cat.Tables.Append(tbl_OC);
+                cat.Tables.Append(tbl_EP);
+                cat.Tables.Append(tbl_pesoDoc);
+                //Now Close the database
+                ADODB.Connection con = cat.ActiveConnection as ADODB.Connection;
+                if (con != null)
+                    con.Close();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            cat = null;
+            return result;
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
